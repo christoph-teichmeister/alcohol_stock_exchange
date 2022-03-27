c10eb1dad4f59703041a578fb8833efb0e97c1aa
christoph-teichmeister <christoph.teichmeister@gmail.com>
2022-03-27 21:38:02 +0200

    extends market urls

--- market/urls.py
+++ market/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import MarketControlView
 
 urlpatterns = [
-    # path("", views.index, name="index"),
-    # path("<str:room_name>/", views.room, name="room"),
-    path("status", views.market_status, name="market-status"),
+    path("status", views.market_status, name="market_status"),
+    path("reset-market", MarketControlView.reset_market, name="reset_market"),
+    path("controls", MarketControlView.as_view(), name="market_controls"),
 ]
