6be03e44f5104c8c29ed2340a74428ede23a43bd
christoph-teichmeister <christoph.teichmeister@gmail.com>
2022-03-27 21:33:40 +0200

    adds retail_price and price_weight to Beverage

--- beverage/models.py
+++ beverage/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 from beverage.managers import BeverageManager
+from stock_price.models import StockPrice
 
 
 class Beverage(models.Model):
     name = models.CharField(max_length=50)
     number_of_sales = models.IntegerField(default=0)
+    retail_price = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        help_text="Price, for which the beverage has been bought",
+    )
+    price_weight = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        help_text="Price-Weight - calculated by dividing the sum of all prices by the beverages retail_price",
+    )
 
     objects = BeverageManager()
 
@@ -16,3 +27,20 @@ class Beverage(models.Model):
         ordering = [
             "name",
         ]
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.create_default_stock_price_if_not_existent()
+
+        return super().save(force_insert, force_update, using, update_fields)
+
+    def create_default_stock_price_if_not_existent(self):
+        if self.stock_prices.exists():
+            return
+
+        StockPrice.objects.create(price=self.retail_price + 1, beverage=self)
+
+    @property
+    def current_stock_price(self):
+        return self.stock_prices.first().price
