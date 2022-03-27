81c6589e6161201bd938253ccf650d8c43e2fe17
christoph-teichmeister <christoph.teichmeister@gmail.com>
2022-03-27 21:36:49 +0200

    writes MarketControlView

--- market/views.py
+++ market/views.py
@@ -1,6 +1,10 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-from market.services import MarketChartContextService
+from market.services import MarketChartContextService, ResetMarketService
 
 
 def market_status(request):
@@ -13,3 +17,29 @@ def market_status(request):
         template_name="market/status.html",
         context={"data": MarketChartContextService.process()},
     )
+
+
+class MarketControlView(generic.TemplateView):
+    template_name = "market/control.html"
+    http_method_names = [
+        "get",  # for initial template loading
+        "post",  # for reset_market method
+    ]
+
+    @staticmethod
+    def reset_market(request, *args, **kwargs):
+        """
+        TODO CT
+        """
+
+        ResetMarketService.process()
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "market-status",
+            {
+                "type": "update_market_price_chart",
+            },
+        )
+
+        return HttpResponse(200)
