da8f1102035224fd9b7e2dc36ac8c3c9c702471e
christoph-teichmeister <christoph.teichmeister@gmail.com>
2022-03-30 21:17:27 +0200

    removes anything to do with price_weight makes use of get_<model>_model() methods where necessary

--- beverage/admin.py
+++ beverage/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin, messages
+from django.contrib import admin
 
 from beverage.models import Beverage
 from stock_price.admin import StockPriceInlineAdmin
@@ -6,21 +6,7 @@ from stock_price.admin import StockPriceInlineAdmin
 
 @admin.register(Beverage)
 class BeverageAdmin(admin.ModelAdmin):
-    actions = ["recalculate_all_price_weights"]
     inlines = [
         StockPriceInlineAdmin,
     ]
-    list_display = ["name", "retail_price", "price_weight"]
-
-    def recalculate_all_price_weights(self, request, queryset):
-        self.message_user(
-            request=request,
-            message="Your selection was ignored - ALL price-weights have been calculated",
-            level=messages.WARNING,
-        )
-        sum_of_all_price_weights = Beverage.objects.calculate_beverage_price_weight()
-        self.message_user(
-            request=request,
-            message=f"Sum of all price_weights is: {sum_of_all_price_weights}",
-            level=messages.INFO,
-        )
+    list_display = ["name", "retail_price"]
--- beverage/models.py
+++ beverage/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from colorfield.fields import ColorField
 
 from beverage.managers import BeverageManager
-from stock_price.models import StockPrice
+from core.helpers import get_stock_price_model, get_stock_model
+
+StockPrice = get_stock_price_model()
+Stock = get_stock_model()
 
 
 class Beverage(models.Model):
@@ -14,10 +17,16 @@ class Beverage(models.Model):
         decimal_places=2,
         help_text="Price, for which the beverage has been bought",
     )
-    price_weight = models.DecimalField(
+    number_of_sales_before_price_falls_by_one_euro = models.DecimalField(
+        default=1,
         max_digits=4,
         decimal_places=2,
-        help_text="Price-Weight - calculated by dividing the sum of all prices by the beverages retail_price",
+        help_text="Defines how many sales are made before the price has fallen by a full euro. \n"
+        "Note: Price will fall before this number is reached too! \n"
+        "Think of it like the following mathematical function: \n"
+        "f(x) = 1/{number_of_sales_before_price_falls_by_one_euro}x + {retail_price} \n"
+        "where 'x' is the beverages number_of_sales / total_sales. \n"
+        "The price will hence never fall below the retail price but will slide along the linear graph.",
     )
 
     objects = BeverageManager()
@@ -34,6 +43,7 @@ class Beverage(models.Model):
         self, force_insert=False, force_update=False, using=None, update_fields=None
     ):
         self.create_default_stock_price_if_not_existent()
+        self.create_default_stock_if_not_existent()
 
         return super().save(force_insert, force_update, using, update_fields)
 
@@ -43,6 +53,16 @@ class Beverage(models.Model):
 
         StockPrice.objects.create(price=self.retail_price + 1, beverage=self)
 
+    def create_default_stock_if_not_existent(self):
+        if self.stock.exists():
+            return
+
+        Stock.objects.create(amount=10, beverage=self)
+
     @property
     def current_stock_price(self):
         return self.stock_prices.first().price
+
+    @property
+    def current_stock(self):
+        return self.stock.first().amount
