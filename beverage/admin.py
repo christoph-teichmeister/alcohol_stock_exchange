194e9605a42c741d9f98939202834e16c0ee1494
christoph-teichmeister <codename.christoph@gmail.com>
2022-04-03 17:14:45 +0200

    adds StockInlineAdmin

--- beverage/admin.py
+++ beverage/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 from beverage.models import Beverage
 from stock_price.admin import StockPriceInlineAdmin
+from warehouse.admin import StockInlineAdmin
 
 
 @admin.register(Beverage)
 class BeverageAdmin(admin.ModelAdmin):
     inlines = [
+        StockInlineAdmin,
         StockPriceInlineAdmin,
     ]
     list_display = ["name", "retail_price", "number_of_sales"]
--- warehouse/admin.py
+++ warehouse/admin.py
@@ -7,3 +7,8 @@ from warehouse.models import Stock
 class BeverageAdmin(admin.ModelAdmin):
     list_display = ["__str__", "amount", "beverage"]
     readonly_fields = ["beverage"]
+
+
+class StockInlineAdmin(admin.StackedInline):
+    model = Stock
+    extra = 0
